a51ec0f179e47db2196161b4bb75ff043b51ee18
evadi <evadi83@gmail.com>
2014-04-20 00:12:57 +0100

    restructured content_script to be more logical

--- js/content_script.js
+++ js/content_script.js
@@ -1,13 +1,13 @@
-
 //No need to wrap in a window.onload as nothing fires when the page is loaded
 //but we do need to be ready to recieve requests straight away
 
+//Defines the actions that can be performed
 var actions = Object.freeze({
    RELOAD: "reload",
    FORCERELOAD: "force"
 });
 
-//
+//Defines the ways in which data can be displayed to the user
 var display = Object.freeze({
    CONSOLE: function (data) {
       console.log(data, "Showing hidden fields");
@@ -17,38 +17,79 @@ var display = Object.freeze({
    }
 });
 
+//Cached version of the UI builder we are using
+var uiBuilder;
+
+//Only when the page has loaded can elements be added to the DOM
 window.onload = function() {
-   var btn = document.createElement("BUTTON")
-   var t = document.createTextNode("CLICK ME");
-   btn.appendChild(t);
-   //Appending to DOM
-   document.body.appendChild(btn);
+   uiBuilder = new UIBuilder();
+   uiBuilder.showReloadOption();
+   uiBuilder.showReloadOption();
+};
+
+//Used to perform various operations on the page
+var pageManager = {
+  
+   //sets the default for displaying data
+   displayTarget: display.CONSOLE,
    
-   btn.onclick = forceReload;
+   //main function used to display data to correct display target
+   displayData: function(displayType) {
+      if (displayType !== undefined) {
+         this.displayTarget = display[displayType];
+      }
+      var data = this.gatherData();
+      this.displayTarget(data);
+   },
+   
+   //scrapes the page gathering information about hidden fields and returns
+   gatherData: function () {
+      return new dataReader().getHiddenFields();
+   },
+  
+   //A method that can be initiated by any UI element which forces a reload
+   forceReload: function () {
+      chrome.extension.sendRequest({ action:actions.FORCERELOAD });
+   }
+   
 };
 
 
-function showFields (displayTarget) {
-   var data = gatherData();
-   display[displayTarget](data);
-}
+//Used to read various parts of the associated page
+var dataReader = function() {
+   
+   this.getHiddenFields = function () {
+      return { name: "David" };
+   };
+   
+};
 
-function gatherData () {
-   return { name: "David" };
-}
-
-function injectScript(func) {
-   var actualCode = '(' + func + ')();'
-   var script = document.createElement('script');
-   script.textContent = actualCode;
-   (document.head||document.documentElement).appendChild(script);
-   script.parentNode.removeChild(script);
-}
-
-//A method that can be initiated by any UI element which forces a reload
-function forceReload () {
-   chrome.extension.sendRequest({ action:actions.FORCERELOAD });
-}
+//Used to generate various UI elements for the current page
+var UIBuilder = function() {
+   this._reloadOptionCreated = false;
+   
+   //display the reload button used to regenerate data
+   this.showReloadOption = function () {
+      if (this._reloadOptionCreated === false) {
+         var btn = document.createElement("BUTTON");
+         var t = document.createTextNode("R");
+         btn.appendChild(t);
+         btn.style.position = "absolute";
+         btn.style.bottom = 0;
+         btn.style.right = 0;
+         //Appending to DOM
+         document.body.appendChild(btn);
+         
+         btn.onclick = pageManager.forceReload;
+      }
+      this._reloadOptionCreated = true;
+   };
+   
+   this.showDataOverlay = function () {
+      
+   };
+   
+};
 
 
 /* Chrome API's */
@@ -56,6 +97,6 @@ function forceReload () {
 //Listen for messages from the controller
 chrome.runtime.onMessage.addListener(function(request, sender, response) {
    if (request.action == actions.RELOAD) {
-      showFields(request.target);
+      pageManager.displayData(request.target);
    }
 });
